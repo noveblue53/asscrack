b828a65b6d764b92f9815b0d1c2c0b94ab88a4a0
noveblue <xandercovington2004@gmail.com>
2026-02-19 00:12:42 -0500

    testing with libp2p chat example

--- src/network/receiver.js
+++ src/network/receiver.js
@@ -1,58 +1,33 @@
-// imports
-import process from 'node:process'
-import { createLibp2p } from 'libp2p'
-import { tcp } from '@libp2p/tcp'
-import { noise } from '@chainsafe/libp2p-noise'
-import { yamux } from '@chainsafe/libp2p-yamux'
-import { multiaddr } from '@multiformats/multiaddr'
-import { ping } from '@libp2p/ping'
+import { createLibp2p } from './libp2p.js'
+import { stdinToStream, streamToConsole } from './stream.js'
 
-//establish a node's setup
-const main =  async () => {
-    const node = await createLibp2p({
-        addresses: {
-         // add a listen address (localhost) to accept TCP connections on a random port
-         listen: ['/ip4/127.0.0.1/tcp/0']
-    },
-        transports: [tcp()],
-        connectionEncryption: [noise()],
-        streamMuxers: [yamux()],
-        services: {
-            ping: ping({
-                protocolPrefix: 'ipfs',
-            }),
-        },
-    })
-
-    // start libp2p
-    await node.start()
-    console.log('libp2p has started')
-
-    // print listening addresses
-    console.log('listening on addresses:')
-    node.getMultiaddrs().forEach((addr) => {
-        console.log(addr.toString())
-    })
-
-    // ping peer if received multi address
-    if (process.argv.length >= 3) {
-        const ma = multiaddr(process.argv[2])
-        console.log(`pinging remote peer at ${process.argv[2]}`)
-        const latency = await node.services.ping.ping(ma)
-        console.log(`pinged ${process.argv[2]} in ${latency}ms`)
-    } else {
-        console.log('no remote peer address given, ping skipped')
+async function run () {
+  // Create a new libp2p node with the given multi-address
+  const listener = await createLibp2p({
+    addresses: {
+      listen: ['/ip4/127.0.0.1/tcp/0']
     }
+  })
 
-const stop = async () => {
-    // stop libp2p
-    await node.stop()
-    console.log('libp2p has stopped')
-    process.exit(0)
+  // Log a message when a remote peer connects to us
+  listener.addEventListener('peer:connect', (evt) => {
+    const remotePeer = evt.detail
+    console.log('connected to: ', remotePeer.toString())
+  })
+
+  // Handle messages for the protocol
+  await listener.handle('/chat/1.0.0', async (stream) => {
+    // Send stdin to the stream
+    stdinToStream(stream)
+    // Read the stream and output to console
+    streamToConsole(stream)
+  })
+
+  // Output listen addresses to the console
+  console.log('Listener ready, listening on:')
+  listener.getMultiaddrs().forEach((ma) => {
+    console.log(ma.toString())
+  })
 }
 
-process.on('SIGTERM', stop)
-process.on('SIGINT', stop)
-}
-
-main().catch(console.error)
+run()
---
+++ src/network/stream.js
@@ -0,0 +1,25 @@
+import { lpStream } from '@libp2p/utils'
+import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
+
+export function stdinToStream (stream) {
+  // Encode with length prefix (so receiving side knows how much data is coming)
+  const lp = lpStream(stream)
+
+  process.stdin.addListener('data', (buf) => {
+    lp.write(buf)
+  })
+}
+
+export function streamToConsole (stream) {
+  const lp = lpStream(stream)
+
+  Promise.resolve().then(async () => {
+    while (true) {
+      // Read from the stream
+      const message = await lp.read()
+
+      // Output the data as a utf8 string
+      console.log('> ' + uint8ArrayToString(message.subarray()).replace('\n', ''))
+    }
+  })
+}
