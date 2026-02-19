6f1d33270e2b14aae8e16d120dda778949606d3f
noveblue <xandercovington2004@gmail.com>
2026-02-19 01:10:12 -0500

    receiver end of basic chat set up, removed old libp2p example

--- src/network/receiver.js
+++ src/network/receiver.js
@@ -1,33 +1,38 @@
-import { createLibp2p } from './libp2p.js'
-import { stdinToStream, streamToConsole } from './stream.js'
+import { createLibp2p } from 'libp2p'
+import { tcp } from '@libp2p/tcp'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { noise } from '@chainsafe/libp2p-noise'
+import { pipe } from 'it-pipe'
+import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
+import * as readline from 'readline'
 
-async function run () {
-  // Create a new libp2p node with the given multi-address
-  const listener = await createLibp2p({
-    addresses: {
-      listen: ['/ip4/127.0.0.1/tcp/0']
+const node = await createLibp2p({
+  addresses: {
+    listen: ['/ip4/127.0.0.1/tcp/0']
+  },
+  transports: [
+    tcp()
+  ],
+  streamMuxers: [
+    yamux()
+  ],
+  connectionEncryption: [
+    noise()
+  ]
+})
+
+await node.start()
+
+console.log('Receiver started with id:', node.peerId.toString())
+console.log('Listening on addresses:', node.getMultiaddrs().forEach(addr => console.log(addr.toString())))
+
+node.handle('/asscrack/chat/1.0.0', async ({ stream }) => {
+  pipe(
+    stream,
+    async function (source) {
+      for await (const msg of source) {
+        console.log('> ', uint8ArrayToString(msg.subarray()))
+      }
     }
-  })
-
-  // Log a message when a remote peer connects to us
-  listener.addEventListener('peer:connect', (evt) => {
-    const remotePeer = evt.detail
-    console.log('connected to: ', remotePeer.toString())
-  })
-
-  // Handle messages for the protocol
-  await listener.handle('/chat/1.0.0', async (stream) => {
-    // Send stdin to the stream
-    stdinToStream(stream)
-    // Read the stream and output to console
-    streamToConsole(stream)
-  })
-
-  // Output listen addresses to the console
-  console.log('Listener ready, listening on:')
-  listener.getMultiaddrs().forEach((ma) => {
-    console.log(ma.toString())
-  })
-}
-
-run()
+  )
+})
